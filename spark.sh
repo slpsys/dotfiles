c526b4d40e316943fcd98d78e1533bae3123979e
Marc Bollinger <marc@lumoslabs.com>
2016-05-14 09:59:44 -0700

    Let's grab more memory

--- spark.sh
+++ spark.sh
@@ -7,6 +7,6 @@
 export SPARK_KEY_PAIR_FILE=$HOME/.ssh/ec2-keypair-file
 export SPARK_KEY_PAIR_NAME=ec2-keypair-name
 export SPARK_EMR_LOG_PATH=s3://your-bucket/logs
-export SPARK_CLUSTER_MASTER_SIZE=m3.2xlarge
-export SPARK_CLUSTER_INSTANCE_SIZE=m3.2xlarge
+export SPARK_CLUSTER_MASTER_SIZE=r3.2xlarge
+export SPARK_CLUSTER_INSTANCE_SIZE=r3.2xlarge
 export SPARK_CLUSTER_INSTANCE_COUNT=10
